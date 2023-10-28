e00f2e1a133a6a133dcfa987b3b6280cee634cad
Georgetsn29 <giotsnobiladze5@gmail.com>
2023-10-28 21:37:53 +0400

    semi done

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+
+btn1.addEventListener('click', function onClick() {
+  btn1.style.backgroundColor = '#fb7413'
+})
+
+btn2.addEventListener('click', function onClick() {
+    btn1.style.backgroundColor = '#fb7413'
+    btn2.style.backgroundColor = '#fb7413'
+  })
+
+  btn3.addEventListener('click', function onClick() {
+    btn1.style.backgroundColor = '#fb7413'
+    btn2.style.backgroundColor = '#fb7413'
+    btn3.style.backgroundColor = '#fb7413'
+  })
+
+  btn4.addEventListener('click', function onClick() {
+    btn1.style.backgroundColor = '#fb7413'
+    btn2.style.backgroundColor = '#fb7413'
+    btn3.style.backgroundColor = '#fb7413'
+    btn4.style.backgroundColor = '#fb7413'
+  })
+
+  btn5.addEventListener('click', function onClick() {
+    btn1.style.backgroundColor = '#fb7413'
+    btn2.style.backgroundColor = '#fb7413'
+    btn3.style.backgroundColor = '#fb7413'
+    btn4.style.backgroundColor = '#fb7413'
+    btn5.style.backgroundColor = '#fb7413'
+  })
